75bf93968d1c15db02dfdf6c2ab224b08228d2de
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-05-06 19:08:41 -0400

    added new file and some detail in js file

---
+++ main.js
@@ -0,0 +1,18 @@
+function reverseString(){
+  let word = prompt("write the string you want")
+  reverseWord= word.split('').reverse().join('')
+  alert(reverseWord)
+}
+
+function fizzBuzz(){
+  let i = 1
+  while (i != 101){
+    if (i%3 == 0 && i%5==0){
+      console.log("Fizz")
+    }else{
+
+      console.log(i);
+    }
+    i++
+  }
+}
